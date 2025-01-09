2617bbd200bcc4668a44e50ed21d332e7c10521e
nkwiecienn <natalia.kwiecien.nk@gmail.com>
2025-01-09 16:51:35 +0100

    fixed absence

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { CalendarViewComponent } from './calendar/calendar-view/calendar-view.component';
 import { AvailabilityHomeComponent } from './availability/availability-home/availability-home.component';
 import { AbsenceComponent } from './absence/absence.component'; // Import komponentu bez lazy loadingu
+import { BasketHomeComponent } from './basket/basket-home/basket-home.component'; // Import komponentu bez lazy loadingu
 
 export const routes: Routes = [
   {
@@ -15,6 +16,10 @@ export const routes: Routes = [
   {
     path: 'calendar',
     component: CalendarViewComponent // Bez lazy loadingu
+  },
+  {
+    path: 'basket',
+    component: BasketHomeComponent // Bez lazy loadingu
   },
   {
     path: '', redirectTo: 'calendar', pathMatch: 'full' // Domyślne przekierowanie na /calendar
---
+++ src/app/basket/basket-home/basket-home.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basket-home',
+  imports: [],
+  templateUrl: './basket-home.component.html',
+  styleUrl: './basket-home.component.css'
+})
+export class BasketHomeComponent {
+
+}
